a3cf78ae34e2833780f80ed6274e4ebc28595fee
Sayer K <sklein5890@student.pps.net>
2024-10-10 11:37:08 -0700

    Walking and wall colision

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,26 +9,39 @@ private float horizontalInput = 0;
 private float verticalInput = 0;
 public int movementSpeed;
 
+Rigidbody2D rb;
+
+bool touchesWall;
+public Transform groundChecker;
+public LayerMask groundMask;
+public float radius;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+    
         GetPlayerInput();
+        MovePlayer();
+        
+        touchesWall = Physics2D.OverlapCircle(groundChecker.position, radius, groundMask);
     }
     
     private void GetPlayerInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = verticalInput.GetAxisRaw("Vertical");
+        verticalInput = Input.GetAxisRaw("Vertical");
     }
 
-    private void MovePlayer(){
-        Vector3 directionVector = new Vector3(horizontalInput, verticalInput, 0;)
-        transform.translate(directionVector.normalized * Time.deltaTime * movementSpeed);
+    private void MovePlayer()
+    {
+        Vector3 directionVector = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(directionVector.normalized * Time.deltaTime * movementSpeed);
+        
     }
 }
