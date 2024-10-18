aa8af2e98bdfc5aba4f98b3e6e244bbfa4b1010d
Sayer K <sklein5890@student.pps.net>
2024-10-18 11:11:44 -0700

    basic enemy

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class testmove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float movementSpeed = 21f;
+    private Rigidbody2D rb;
+    private Vector2 movementDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate() {
+        rb.velocity = movementDirection * movementSpeed;
+    }
+}
