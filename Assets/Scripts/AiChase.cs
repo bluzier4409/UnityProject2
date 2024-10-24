347ba639deaa6c083009c33390ae49e12aaab57a
Sayer K <sklein5890@student.pps.net>
2024-10-24 11:59:00 -0700

    some fixes to player and enemy following

--- Assets/Scripts/AiChase.cs
+++ Assets/Scripts/AiChase.cs
@@ -8,27 +8,28 @@ public class AiChase : MonoBehaviour
     public GameObject player;
     public float speed;
     public float touchingDistance;
+    private Rigidbody2D rb;
 
     private float distance;
-    // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
 
-    
+       
 
         if(distance > touchingDistance){
                     transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-
+                     
         }
     }
+
+    
 
 
 }
