9b257f5b6983cbe6b7bf194af0a9cfc7e852bd51
Sayer K <sklein5890@student.pps.net>
2024-10-25 10:31:58 -0700

    roating rat and flipping player

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.ShaderGraph.Internal;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class testmove : MonoBehaviour
@@ -9,6 +10,8 @@ public class testmove : MonoBehaviour
     [SerializeField] private float movementSpeed = 21f;
     private Rigidbody2D rb;
     private Vector2 movementDirection;
+
+    bool facingRight = false;
 
     void Start()
     {
@@ -23,5 +26,19 @@ public class testmove : MonoBehaviour
 
     void FixedUpdate() {
         rb.velocity = movementDirection * movementSpeed;
+
+        if (movementDirection.x > 0 && !facingRight){
+            Flip();
+        }
+        else if (movementDirection.x < 0 && facingRight){
+            Flip();
+        }
+    }
+
+    void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
     }
 }
