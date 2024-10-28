ff61130561902cef85f052c75b0443a1afb08ef4
Ben <bluzier4409@student.pps.net>
2024-10-28 10:12:22 -0700

    Fix

--- Assets/Scripts/MeleeCard.cs
+++ Assets/Scripts/MeleeCard.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeleeCard : Card
+public class MeleeCard : MonoBehaviour
 {
-    private float _damage;
-    private float _range;
+ //   private float _damage;
+  //  private float _range;
     
-    public MeleeCard() : base(false)
-    {
-        
-    }
+  //  public MeleeCard() : base(false)
+   // {
+   //     
+  //  }
     
-    public MeleeCard(float damage, float range)
-    {
-        _damage = damage;
-        _range = range;
-    }
+    //public MeleeCard(float damage, float range)
+   // {
+   //     _damage = damage;
+   //     _range = range;
+   // }
     
 }
