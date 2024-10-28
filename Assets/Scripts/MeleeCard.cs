5cbb295d5ab94020f852004e853e02689b8bed82
githubthrowaway7 <githu2qihdjasghf@gmail.com>
2024-10-28 10:14:32 -0700

    irrelevant

--- Assets/Scripts/MeleeCard.cs
+++ Assets/Scripts/MeleeCard.cs
@@ -7,10 +7,17 @@ public class MeleeCard : MonoBehaviour
  //   private float _damage;
   //  private float _range;
     
+<<<<<<< Updated upstream
   //  public MeleeCard() : base(false)
    // {
    //     
   //  }
+=======
+    public MeleeCard() : base(false)
+    {
+        return;
+    }
+>>>>>>> Stashed changes
     
     //public MeleeCard(float damage, float range)
    // {
