71ec4591c85f760de08c279028dff1bdc3e2ddf9
Ben <bluzier4409@student.pps.net>
2024-10-30 09:53:28 -0700

    Script Change

--- Assets/Scripts/MeleeCard.cs
+++ Assets/Scripts/MeleeCard.cs
@@ -4,25 +4,5 @@ using UnityEngine;
 
 public class MeleeCard : MonoBehaviour
 {
- //   private float _damage;
-  //  private float _range;
-    
-<<<<<<< Updated upstream
-  //  public MeleeCard() : base(false)
-   // {
-   //     
-  //  }
-=======
-    public MeleeCard() : base(false)
-    {
-        return;
-    }
->>>>>>> Stashed changes
-    
-    //public MeleeCard(float damage, float range)
-   // {
-   //     _damage = damage;
-   //     _range = range;
-   // }
-    
+
 }
