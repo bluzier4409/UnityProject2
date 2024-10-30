d01ccacf64c7f7d83184a0422e0e79b098ac86e3
Sayer K <sklein5890@student.pps.net>
2024-10-30 12:02:58 -0700

    health payer

--- Assets/Scripts/PlayersScript/PlayerMove.cs
+++ Assets/Scripts/PlayersScript/PlayerMove.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 
 public class testmove : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] private float movementSpeed = 21f;
     private Rigidbody2D rb;
     private Vector2 movementDirection;
@@ -18,7 +17,6 @@ public class testmove : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
