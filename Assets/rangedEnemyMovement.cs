05f8ed0e9110813466b52b08bbdaea8482efb4dd
Sayer K <sklein5890@student.pps.net>
2024-11-20 12:02:47 -0800

    rangered

---
+++ Assets/rangedEnemyMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class rangedEnemyMovement : MonoBehaviour
+{
+    public GameObject player;
+
+    public float speed;
+    public float touchingDistance;
+
+    public float detectionDistance;
+    private Rigidbody2D rb;
+
+    private float distance;
+
+    private bool seen = false;
+    bool facingRight = false;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+//only happens if inside of detection disctance
+
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+
+        if(distance < detectionDistance){
+            seen = true;
+        }
+       
+
+        if(distance > touchingDistance && seen == true){
+                    transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+                     
+        }
+
+        
+    }
+
+    public bool getTouching(){
+        return distance <= touchingDistance;
+    }
+
+    void Flip(){
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
+    }
+
+
+}
