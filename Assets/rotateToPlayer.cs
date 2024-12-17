ffc8aa6fde3adc1bdccb5a9c1d1af54304c5d185
Sayer K <sklein5890@student.pps.net>
2024-12-17 11:39:08 -0800

    trying to find how to rotate at end of path,
    i think im going to make my own AIpath script

--- Assets/rotateToPlayer.cs
+++ Assets/rotateToPlayer.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class rotateToPlayer : MonoBehaviour
 {
+    float smooth = 5.0f;
+    float tiltAngle = 60.0f;
     public GameObject player;
     void Update()
     {
-        Vector2 direction = player.transform.position - transform.position;
+            
+        Vector3 difference = player.GetComponent<Transform>().position - transform.position;
+        float rotationZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, rotationZ + 180), Time.deltaTime);
+
+
+        /*Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+        transform.rotation = Quaternion.Euler(Vector3.forward * angle);*/
     }
 }
