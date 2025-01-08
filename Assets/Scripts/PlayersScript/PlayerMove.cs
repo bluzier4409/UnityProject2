6ca635e1971945dd11fb42179cf33629eae3c093
Ben <bluzier4409@student.pps.net>
2025-01-08 12:03:36 -0800

    commit 1/8/25
    working on char anims

--- Assets/Scripts/PlayersScript/PlayerMove.cs
+++ Assets/Scripts/PlayersScript/PlayerMove.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 //using UnityEditor.ShaderGraph.Internal;
 //using UnityEditor.Tilemaps;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class playerMove : MonoBehaviour
 {
@@ -14,18 +16,30 @@ public class playerMove : MonoBehaviour
     private bool dodgeOnCooldown;
 
    // [SerializeField] private float dodgeSpeed = 21f;
-
-
-    bool facingRight = false;
+   
+    
+    bool north = false;
+    bool northeast = false;
+    bool east = false;
+    bool southeast = false;
+    bool south = false;
+    bool southwest = false;
+    bool west = false;
+    bool northwest = false;
+    
+    bool isMoving = false;
+    
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     void Update()
     {
-        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
         rb.velocity = movementDirection * movementSpeed;
 
         if(dodgeOnCooldown){
@@ -46,12 +60,14 @@ public class playerMove : MonoBehaviour
     }
 
     void FixedUpdate() {
+        Vector2 basevec = new Vector2(0, 1);
+        float angle = Vector2.SignedAngle(basevec, movementDirection);
 
 
-        if (movementDirection.x > 0 && !facingRight){
+        if (movementDirection.x> 0 && !east){
             Flip();
         }
-        else if (movementDirection.x < 0 && facingRight){
+        else if (movementDirection.x < 0 && east){
             Flip();
         }
     }
@@ -60,7 +76,7 @@ public class playerMove : MonoBehaviour
         Vector3 currentScale = gameObject.transform.localScale;
         currentScale.x *= -1;
         gameObject.transform.localScale = currentScale;
-        facingRight = !facingRight;
+        east = !east;
     }
 
     void Dodge(){
