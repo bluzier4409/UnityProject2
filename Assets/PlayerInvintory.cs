151afa9b04d414a83cb712c6d4ea865309528500
Sayer K <sklein5890@student.pps.net>
2025-02-06 12:03:44 -0800

    key UI

--- Assets/PlayerInvintory.cs
+++ Assets/PlayerInvintory.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInvintory : MonoBehaviour
 {
+
     private int numKeysHeld = 0;
+    public Text keyText;
+
+    void Start(){
+        keyText.text = numKeysHeld.ToString();
+    }
 
     public void giveKey(){
         numKeysHeld++;
         Debug.Log("You now have "+ numKeysHeld+" keys.");
+        keyText.text = numKeysHeld.ToString();
+
     }
     public int GetNumKeysHeld(){
         return numKeysHeld;
