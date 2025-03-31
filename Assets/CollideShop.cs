fc91d30ab8f9e0565d884f9713b0b6ecbf097c75
githubthrowaway7 <githu2qihdjasghf@gmail.com>
2025-03-31 11:39:23 -0700

    worked on demo scene and fixed shop leaving big

--- Assets/CollideShop.cs
+++ Assets/CollideShop.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class CollideShop : MonoBehaviour {
     Transform theTransform  = null;
+    ShowHidePressE theScript = null;
     private void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("Collided With Shop");
         GameObject collidedObject = other.gameObject;
         Transform pressETransform = collidedObject.transform.Find("PressE");
         theTransform=pressETransform;
+        
         if (pressETransform != null) {
             // Get the ShowHidePressE script attached to the PressE object
             ShowHidePressE pressEScript = pressETransform.GetComponent<ShowHidePressE>();
-
+            theScript =  pressEScript;
             if (pressEScript != null) {
                 // Call a function from the ShowHidePressE script
                 pressEScript.Show();
                 pressEScript.SetPlayerInRange(true);
-                Debug.Log("Found");
+                Debug.Log("Setting True");
                 // or you can call any other function you need from ShowHidePressE
             } else {
                 Debug.LogWarning("ShowHidePressE script not found on PressE object.");
@@ -33,7 +35,9 @@ public class CollideShop : MonoBehaviour {
         Transform pressETransform = collidedObject.transform.Find("PressE");
 
         ShowHidePressE pressEScript = theTransform.GetComponent<ShowHidePressE>();
-        pressEScript.Hide();
+        //theScript.HidePurchase();
+        if(theScript) Debug.Log("Exists");
+        //pressEScript.HidePurchase();
 
         // Find the PressE object within the collided object (which should be the player prefab)
         
@@ -44,9 +48,10 @@ public class CollideShop : MonoBehaviour {
 
             if (pressEScript != null) {
                 // Hide the "PressE" when leaving the shop area
-                pressEScript.Hide();
-                Debug.Log("OORange");
+                pressEScript.HidePurchase();
+                Debug.Log("Setting False");
                 pressEScript.SetPlayerInRange(false);
+                theScript.SetPlayerInRange(false);
             }
         }
     }
