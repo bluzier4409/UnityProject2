40a8f067d9ef043d618b15006d73ab2ae2b307c8
Sayer K <sklein5890@student.pps.net>
2025-04-30 12:02:32 -0700

    working on bomb
    deal dmg when on certain frame

--- Assets/explosionDMG.cs
+++ Assets/explosionDMG.cs
@@ -4,33 +4,46 @@ using UnityEngine;
 
 public class explosionDMG : MonoBehaviour
 {
+
+    private Vector3 whereIAm;
+
+    private double radius;
+    [SerializeField]private int maxExplosionDmg_oneGreater = 11;
+
+    //chnage radius as it goes?
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enabled = false;
+        whereIAm = this.transform.position;
+        radius = GetComponent<CircleCollider2D>().radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-void AreaDamageEnemies(Vector3 location, float radius, float damage)
-{
-    Collider[] objectsInRange = Physics.OverlapSphere(location, radius);
-    foreach (Collider col in objectsInRange)
+    public void enableCollider(){
+        enabled = true;
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        ObjHealth enemy = col.GetComponent<ObjHealth>();
+        ObjHealth enemy = collision.GetComponent<ObjHealth>();
         if (enemy != null)
         {
-            // linear falloff of effect
-            float proximity = (location - enemy.transform.position).magnitude;
-            float effect = 1 - (proximity / radius);
-
-            //enemy.Damage(damage * effect);
+            int proximity = (int)(whereIAm - enemy.transform.position).magnitude;
+            Debug.Log("prox =  " + proximity);
+            int dmgAmount = (int)(maxExplosionDmg_oneGreater - (proximity/radius));
+            enemy.Damage(dmgAmount);
+            Debug.Log("did " + dmgAmount + "damage prox");
         }
+        enabled = false;
     }
-}
 
 }
